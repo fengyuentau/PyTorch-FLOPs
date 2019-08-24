002cbfd8499db6d3c7457bc4ed9f3639ce4c70f4
unknown <yuentau.feng@gmail.com>
2019-08-25 03:07:42 +0800

    Added test modules on conv, act, eltops

--- test/test_conv.py
+++ test/test_conv.py
@@ -1,33 +1,59 @@
-# Things need to be test:
-# 1. whether the output size is correct
-# 2. whether the output flops is correct
+from _utils import test_on
 
 import sys
-sys.path.append('..')
-
+sys.path.append('.')
 from flops_counter import nn
 
-test_convs = [
-    nn.Conv2d(3, 128, 3, 1, 1), # remain the same size but with channels changed
-    nn.Conv2d(3, 128, 3, 2, 1), # height/width becomes half of the original
-    nn.Conv2d(3, 512, 1, 1, 0) # remain the same size but with channels changed
-]
-test_convtrans = [
-    nn.ConvTranspose2d(3, 128, 4, 2, 1)
-]
 
-test_ins = [
-    [3, 64, 64],
-    [3, 14, 14],
-    [3, 7, 7]
-]
+######
+# test on Conv2d
+######
+conv = {
+    'layers': [
+        nn.Conv2d(3, 64, 7, 2, 3, bias=False), # half of the shape
+        nn.Conv2d(64, 64, 1, 1, 0, bias=False), # same shape
+        nn.Conv2d(64, 64, 3, 1, 1, bias=False) # same shape
+    ],
+    'ins': [
+        [3, 224, 224],
+        [64, 56, 56],
+        [64, 56, 56]
+    ],
+    'out_shape': [
+        [64, 112, 112],
+        [64, 56, 56],
+        [64, 56, 56]
+    ],
+    'out_flops': [
+        235225088,
+        25489408,
+        231010304
+    ]
+}
 
-for tin in test_ins:
-    for tconv in test_convs:
-        output, flops = tconv(tin)
-        print('{:s}, input size: {:s}, output size: {:s}, flops: {:d}'.format(str(tconv), str(tin), str(output), flops))
-    for tconvtran in test_convtrans:
-        output, flops = tconvtran(tin)
-        print('{:s}, input size: {:s}, output size: {:s}, flops: {:d}'.format(str(tconvtran), str(tin), str(output), flops))
-        
-    print()
+test_on(conv)
+
+
+######
+# test on ConvTranspose2d
+######
+convtran = {
+    'layers': [
+        nn.ConvTranspose2d(512, 256, 4, 2, 1), # double the shape, except channels
+        nn.ConvTranspose2d(1024, 256, 4, 4, 0) # quadro the shape, except channels
+    ],
+    'ins': [
+        [512, 28, 28],
+        [1024, 14, 14]
+    ],
+    'out_shape': [
+        [256, 56, 56],
+        [256, 56, 56]
+    ],
+    'out_flops': [
+        13153337344,
+        26306674688
+    ]
+}
+
+test_on(convtran)

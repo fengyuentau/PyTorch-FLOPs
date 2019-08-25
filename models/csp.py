1f1a5c6551e95bfe98183366d1a7a689d7f2b445
unknown <yuentau.feng@gmail.com>
2019-08-26 00:46:17 +0800

    CSP now inherit from nn.Module

--- models/csp.py
+++ models/csp.py
@@ -4,117 +4,91 @@ sys.path.append('..')
 import flops_counter
 import flops_counter.nn as nn
 
-class IdentityBlock(object):
+class IdentityBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1):
-        # self.as_super = super(IdentityBlock, self)
-        # self.as_super.__init__()
         super(IdentityBlock, self).__init__()
 
         out_channels_1, out_channels_2, out_channels_3 = out_channels//4, out_channels//4, out_channels
 
-        self.conv1 = nn.Conv2d(in_channels, out_channels_1, kernel_size=(1, 1), bias=False)
+        self.conv1 = nn.Conv2d(in_channels, out_channels_1, kernel_size=(1, 1))
         self.bn1 = nn.BatchNorm2d(out_channels_1)
         self.relu1 = nn.ReLU(inplace=True)
 
-        self.conv2 = nn.Conv2d(out_channels_1, out_channels_2, kernel_size=(kernel_size, kernel_size), padding=(padding, padding), dilation=(dilation, dilation), bias=False)
+        self.conv2 = nn.Conv2d(out_channels_1, out_channels_2, kernel_size=(kernel_size, kernel_size), padding=(padding, padding), dilation=(dilation, dilation))
         self.bn2 = nn.BatchNorm2d(out_channels_2)
         self.relu2 = nn.ReLU(inplace=True)
 
-        self.conv3 = nn.Conv2d(out_channels_2, out_channels_3, kernel_size=(1, 1), bias=False)
+        self.conv3 = nn.Conv2d(out_channels_2, out_channels_3, kernel_size=(1, 1))
         self.bn3 = nn.BatchNorm2d(out_channels_3)
 
         self.eltadd = nn.EltAdd()
         self.relu_f = nn.ReLU(inplace=True)
 
-    def __call__(self, x):
-        flops = int(0)
+    def forward(self, x):
         identity = x
 
-        out, flops_t = self.conv1(x)
-        flops += flops_t
-        out, flops = self.bn1(out)
-        flops += flops_t
-        out, flops = self.relu1(out)
-        flops += flops_t
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu1(out)
 
-        out, flops = self.conv2(out)
-        flops += flops_t
-        out, flops = self.bn2(out)
-        flops += flops_t
-        out, flops = self.relu2(out)
-        flops += flops_t
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu2(out)
 
-        out, flops = self.conv3(out)
-        flops += flops_t
-        out, flops = self.bn3(out)
-        flops += flops_t
+        out = self.conv3(out)
+        out = self.bn3(out)
 
-        out, flops = self.eltadd(identity, out)
-        flops += flops_t
-        out, flops = self.relu_f(out)
-        flops += flops_t
+        out = self.eltadd(identity, out)
+        out = self.relu_f(out)
 
-        return out, flops
+        return out
 
-class ConvBlock(object):
+class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1):
         super(ConvBlock, self).__init__()
 
         out_channels_1, out_channels_2, out_channels_3 = out_channels//4, out_channels//4, out_channels
 
-        self.conv1 = nn.Conv2d(in_channels, out_channels_1, kernel_size=(1, 1), stride=(stride, stride), bias=False)
+        self.conv1 = nn.Conv2d(in_channels, out_channels_1, kernel_size=(1, 1), stride=(stride, stride))
         self.bn1 = nn.BatchNorm2d(out_channels_1)
         self.relu1 = nn.ReLU(inplace=True)
 
-        self.conv2 = nn.Conv2d(out_channels_1, out_channels_2, kernel_size=(kernel_size, kernel_size), padding=(padding, padding), dilation=(dilation, dilation), bias=False)
+        self.conv2 = nn.Conv2d(out_channels_1, out_channels_2, kernel_size=(kernel_size, kernel_size), padding=(padding, padding), dilation=(dilation, dilation))
         self.bn2 = nn.BatchNorm2d(out_channels_2)
         self.relu2 = nn.ReLU(inplace=True)
 
-        self.conv3 = nn.Conv2d(out_channels_2, out_channels_3, kernel_size=(1, 1), bias=False)
+        self.conv3 = nn.Conv2d(out_channels_2, out_channels_3, kernel_size=(1, 1))
         self.bn3 = nn.BatchNorm2d(out_channels_3)
 
-        self.conv_shortcut = nn.Conv2d(in_channels, out_channels_3, kernel_size=(1, 1), stride=(stride, stride), bias=False)
+        self.conv_shortcut = nn.Conv2d(in_channels, out_channels_3, kernel_size=(1, 1), stride=(stride, stride))
         self.bn_shortcut = nn.BatchNorm2d(out_channels_3)
 
         self.eltadd = nn.EltAdd()
         self.relu_f = nn.ReLU(inplace=True)
 
-    def __call__(self, x):
-        flops = int(0)
+    def forward(self, x):
         identity = x
 
-        out, flops_t = self.conv1(x)
-        flops += flops_t
-        out, flops_t = self.bn1(out)
-        flops += flops_t
-        out, flops_t = self.relu1(out)
-        flops += flops_t
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu1(out)
 
-        out, flops_t = self.conv2(out)
-        flops += flops_t
-        out, flops_t = self.bn2(out)
-        flops += flops_t
-        out, flops_t = self.relu2(out)
-        flops += flops_t
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu2(out)
 
-        out, flops_t = self.conv3(out)
-        flops += flops_t
-        out, flops_t = self.bn3(out)
-        flops += flops_t
+        out = self.conv3(out)
+        out = self.bn3(out)
 
-        identity, flops_t = self.conv_shortcut(x)
-        flops += flops_t
-        identity, flops_t = self.bn_shortcut(identity)
-        flops += flops_t
+        identity = self.conv_shortcut(identity)
+        identity = self.bn_shortcut(identity)
 
-        out, flops_t = self.eltadd(identity, out)
-        flops += flops_t
-        out, flops_t = self.relu_f(out)
-        flops += flops_t
+        out = self.eltadd(identity, out)
+        out = self.relu_f(out)
 
-        return out, flops
+        return out
 
-class CSP(object):
+class CSP(nn.Module):
     def __init__(self):
         super(CSP, self).__init__()
         #####
@@ -122,7 +96,7 @@ class CSP(object):
         #####
         # build resnet50
         # base
-        self.base_conv = nn.Conv2d(3, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        self.base_conv = nn.Conv2d(3, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3))
         self.base_bn = nn.BatchNorm2d(64)
         self.base_relu = nn.ReLU(inplace=True)
         self.base_maxpooling = nn.MaxPool2d(kernel_size=(3, 3), stride=2, padding=1)
@@ -167,12 +141,12 @@ class CSP(object):
         self.p5up_trconv = nn.ConvTranspose2d(2048, 256, kernel_size=4, stride=4, padding=0)
         self.p5up_l2norm = nn.L2Norm2d(256, 10)
         # detection head - feat
-        self.feat_conv = nn.Conv2d(768, 256, kernel_size=3, padding=1, bias=False)
+        self.feat_conv = nn.Conv2d(768, 256, kernel_size=3, padding=1)
         self.feat_bn = nn.BatchNorm2d(256)
         self.feat_relu = nn.ReLU(inplace=True)
         # detection head - class
         self.class_conv = nn.Conv2d(256, 1, kernel_size=1, bias=True)
-        # self.class_sigmoid = nn.Sigmoid()
+        self.class_sigmoid = nn.Sigmoid()
         # detection head - regr
         self.regr_conv = nn.Conv2d(256, 2, kernel_size=1)
         self.regr_relu = nn.ReLU(inplace=True)
@@ -180,101 +154,69 @@ class CSP(object):
         self.offset_conv = nn.Conv2d(256, 2, kernel_size=1)
         self.offset_relu = nn.ReLU(inplace=True)
 
-    def __call__(self, x):
-        flops = int(0)
+    @property
+    def name(self):
+        return super(CSP, self).name + '_ResNet50'
+
+    def forward(self, x):
         # base
-        out, flops_t = self.base_conv(x)
-        flops += flops_t
-        out, flops_t = self.base_bn(out)
-        flops += flops_t
-        out, flops_t = self.base_relu(out)
-        flops += flops_t
-        out, flops_t = self.base_maxpooling(out)
-        flops += flops_t
+        out = self.base_conv(x)
+        out = self.base_bn(out)
+        out = self.base_relu(out)
+        out = self.base_maxpooling(out)
 
         # layer1
-        out, flops_t = self.layer1_bottleneck0(out)
-        flops += flops_t
-        out, flops_t = self.layer1_bottleneck1(out)
-        flops += flops_t
-        out, flops_t = self.layer1_bottleneck2(out)
-        flops += flops_t
+        out = self.layer1_bottleneck0(out)
+        out = self.layer1_bottleneck1(out)
+        out = self.layer1_bottleneck2(out)
 
         # layer2
-        out, flops_t = self.layer2_bottleneck0(out)
-        flops += flops_t
-        out, flops_t = self.layer2_bottleneck1(out)
-        flops += flops_t
-        out, flops_t = self.layer2_bottleneck2(out)
-        flops += flops_t
-        stage3, flops_t = self.layer2_bottleneck3(out)
-        flops += flops_t
+        out = self.layer2_bottleneck0(out)
+        out = self.layer2_bottleneck1(out)
+        out = self.layer2_bottleneck2(out)
+        stage3 = self.layer2_bottleneck3(out)
 
         # layer3
-        out, flops_t = self.layer3_bottleneck0(stage3)
-        flops += flops_t
-        out, flops_t = self.layer3_bottleneck1(out)
-        flops += flops_t
-        out, flops_t = self.layer3_bottleneck2(out)
-        flops += flops_t
-        out, flops_t = self.layer3_bottleneck3(out)
-        flops += flops_t
-        out, flops_t = self.layer3_bottleneck4(out)
-        flops += flops_t
-        stage4, flops_t = self.layer3_bottleneck5(out)
-        flops += flops_t
+        out = self.layer3_bottleneck0(stage3)
+        out = self.layer3_bottleneck1(out)
+        out = self.layer3_bottleneck2(out)
+        out = self.layer3_bottleneck3(out)
+        out = self.layer3_bottleneck4(out)
+        stage4 = self.layer3_bottleneck5(out)
 
         # layer4
-        out, flops_t = self.layer4_bottleneck0(stage4)
-        flops += flops_t
-        out, flops_t = self.layer4_bottleneck1(out)
-        flops += flops_t
-        stage5, flops_t = self.layer4_bottleneck2(out)
-        flops += flops_t
+        out = self.layer4_bottleneck0(stage4)
+        out = self.layer4_bottleneck1(out)
+        stage5 = self.layer4_bottleneck2(out)
 
         # pn_deconv
-        p3up, flops_t = self.p3up_trconv(stage3)
-        flops += flops_t
-        p4up, flops_t = self.p4up_trconv(stage4)
-        flops += flops_t
-        p5up, flops_t = self.p5up_trconv(stage5)
-        flops += flops_t
+        p3up = self.p3up_trconv(stage3)
+        p4up = self.p4up_trconv(stage4)
+        p5up = self.p5up_trconv(stage5)
 
         # l2norm
-        p3up, flops_t = self.p3up_l2norm(p3up)
-        flops += flops_t
-        p4up, flops_t = self.p4up_l2norm(p4up)
-        flops += flops_t
-        p5up, flops_t = self.p5up_l2norm(p5up)
-        flops += flops_t
+        p3up = self.p3up_l2norm(p3up)
+        p4up = self.p4up_l2norm(p4up)
+        p5up = self.p5up_l2norm(p5up)
 
         # concat
         conc = flops_counter.cat((p3up, p4up, p5up), 0)
 
         # detection head - feat
-        feat, flops_t = self.feat_conv(conc)
-        flops += flops_t
-        feat, flops_t = self.feat_bn(feat)
-        flops += flops_t
-        feat, flops_t = self.feat_relu(feat)
-        flops += flops_t
+        feat = self.feat_conv(conc)
+        feat = self.feat_bn(feat)
+        feat = self.feat_relu(feat)
 
         # detection head - class
-        x_class, flops_t = self.class_conv(feat)
-        flops += flops_t
-        # x_class, flops_t = self.class_sigmoid(x_class)
-        # flops += flops_t
+        x_class = self.class_conv(feat)
+        x_class = self.class_sigmoid(x_class)
 
         # detection head - regr
-        x_regr, flops_t = self.regr_conv(feat)
-        flops += flops_t
-        x_regr, flops_t = self.regr_relu(x_regr)
-        flops += flops_t
+        x_regr = self.regr_conv(feat)
+        x_regr = self.regr_relu(x_regr)
 
         # detection head - offset
-        x_offset, flops_t = self.offset_conv(feat)
-        flops += flops_t
-        x_offset, flops_t = self.offset_relu(x_offset)
-        flops += flops_t
+        x_offset = self.offset_conv(feat)
+        x_offset = self.offset_relu(x_offset)
 
-        return flops
+        return x_class, x_regr, x_offset

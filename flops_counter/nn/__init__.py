0102d7bcfe69a8a7cc3d7189b3489e7c48f48cd4
unknown <yuentau.feng@gmail.com>
2019-08-27 09:49:26 +0800

    Added entrance for adaptiveavgpool2d

--- flops_counter/nn/__init__.py
+++ flops_counter/nn/__init__.py
@@ -2,7 +2,7 @@ from .module import Module
 from .convolution import Conv2d, ConvTranspose2d
 from .activation import ReLU, Sigmoid
 from .normalization import BatchNorm2d, L2Norm2d
-from .pooling import MaxPool2d
+from .pooling import MaxPool2d, AdaptiveAvgPool2d
 from .eltops import EltAdd, EltMul
 
 
@@ -11,6 +11,6 @@ __all__ = [
     'Conv2d', 'ConvTranspose2d',
     'ReLU', 'Sigmoid',
     'BatchNorm2d', 'L2Norm2d',
-    'MaxPool2d',
+    'MaxPool2d', 'AdaptiveAvgPool2d',
     'EltAdd', 'EltMul'
 ]

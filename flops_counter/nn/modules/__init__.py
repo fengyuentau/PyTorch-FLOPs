96fd517d979f6ec5613c89d99bc265380283179a
冯远滔 <yuentau.feng@gmail.com>
2019-08-28 16:48:09 +0800

    Add layer Softmax

--- flops_counter/nn/modules/__init__.py
+++ flops_counter/nn/modules/__init__.py
@@ -1,6 +1,6 @@
 from .module import Module
 from .convolution import Conv2d, ConvTranspose2d
-from .activation import ReLU, Sigmoid
+from .activation import ReLU, Sigmoid, Softmax
 from .normalization import BatchNorm2d, L2Norm2d
 from .pooling import MaxPool2d, AdaptiveAvgPool2d
 from .eltops import EltAdd, EltMul
@@ -12,7 +12,7 @@ from .container import Sequential
 __all__ = [
     'Module',
     'Conv2d', 'ConvTranspose2d',
-    'ReLU', 'Sigmoid',
+    'ReLU', 'Sigmoid', 'Softmax',
     'BatchNorm2d', 'L2Norm2d',
     'MaxPool2d', 'AdaptiveAvgPool2d',
     'EltAdd', 'EltMul',
--- flops_counter/nn/modules/activation.py
+++ flops_counter/nn/modules/activation.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 from .module import Module
 
 class ReLU(Module):
@@ -40,6 +42,39 @@ class Sigmoid(Module):
         cin, hin, win = x
         cout, hout, wout = y
         self._flops = 3 * (cout * hout * wout)
+
+    def forward(self, x):
+        '''
+        x should be of shape [channels, height, width]
+        '''
+        assert len(x) == 3, 'input size should be 3, which is [channels, height, width].'
+
+        y = [i for i in x]
+
+        self._calc_flops(x, y)
+
+        return y
+
+class Softmax(Module):
+    __constants__ = ['dim']
+
+    def __init__(self, dim=None):
+        super(Softmax, self).__init__()
+        self.dim = dim
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        if not hasattr(self, 'dim'):
+            self.dim = None
+
+    def extra_repr(self):
+        return 'dim={dim}'.format(dim=self.dim)
+
+    def _calc_flops(self, x, y):
+        ondim = y[self.dim]
+        y_nelements = reduce((lambda a, b: a * b), y)
+        # flops_softmax = ((2 * h * w - 1) + h * w) * c = 3hwc - c
+        self._flops = 3 * y_nelements - ondim
 
     def forward(self, x):
         '''

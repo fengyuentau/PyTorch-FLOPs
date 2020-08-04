494143fd2ca73c9cfc5187e2ff7ce0a47e6f27d8
fytao <yuantao.feng@outlook.com>
2020-08-04 19:47:37 +0800

    update to flops_counter.tensorsize

--- test/models/test_dsfd.py
+++ test/models/test_dsfd.py
@@ -1,14 +1,10 @@
 import sys
 sys.path.append('.')
+import flops_counter as fc
 from models.dsfd import DSFD
 
-dsfd = DSFD()
-
-# x = [3, 64, 64]
-# y = dsfd(x)
-# print(dsfd.flops)
-
-x = [3, 224, 224]
-y = dsfd(x)
-print(y, dsfd.flops)
-print(dsfd)
+model = DSFD()
+x = fc.TensorSize([1, 3, 224, 224])
+y = model(x)
+print(y, model.flops)
+# print(dsfd)
